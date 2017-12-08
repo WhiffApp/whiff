86d81f3b6481248ddcfecb0fcad6c1d007186708
jonlim1994 <jonlim1994@gmail.com>
2017-12-08 09:56:10 +0800

    Added exit button, removed action bar

--- app/src/main/java/fyp/uow/whiff/MainActivity.java
+++ app/src/main/java/fyp/uow/whiff/MainActivity.java
@@ -1,7 +1,10 @@
 package fyp.uow.whiff;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button exitButton  = findViewById(R.id.exitButton);
+
+        exitButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        int pid = android.os.Process.myPid();
+                        android.os.Process.killProcess(pid);
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        startActivity(intent);
+                    }
+                }
+        );
+
     }
 }
