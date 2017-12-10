ea7f4f06992649bf5f96f38075bf05983819388a
jonlim1994 <jonlim1994@gmail.com>
2017-12-10 22:57:40 +0800

    Added Help and FAQ

---
+++ app/src/main/java/fyp/uow/whiff/help_activity.java
@@ -0,0 +1,56 @@
+package fyp.uow.whiff;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class help_activity extends AppCompatActivity {
+
+    private static final String TAG = "help_activity";
+
+    //SectionsPageAdapter.java
+    private SectionsPageAdapter mSectionPageAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_activity);
+        Log.d(TAG, "Help Menu.");
+
+        mSectionPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        //Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager)findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter((getSupportFragmentManager()));
+        adapter.addFragment(new helpFragment(),"Help");
+        adapter.addFragment(new faqFragment(),"FAQ");
+        viewPager.setAdapter(adapter);
+    }
+
+}
