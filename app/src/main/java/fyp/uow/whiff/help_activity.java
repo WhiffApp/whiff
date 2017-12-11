7f28796a3588713a0c05148e6e062ef7087537c4
jonlim1994 <jonlim1994@gmail.com>
2017-12-11 18:04:30 +0800

    Minor changes

--- app/src/main/java/fyp/uow/whiff/help_activity.java
+++ app/src/main/java/fyp/uow/whiff/help_activity.java
@@ -1,24 +1,11 @@
 package fyp.uow.whiff;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class help_activity extends AppCompatActivity {
 
@@ -32,7 +19,7 @@ public class help_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help_activity);
+        setContentView(R.layout.activity_help);
         Log.d(TAG, "Help Menu.");
 
         mSectionPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
