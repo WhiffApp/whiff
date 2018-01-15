bbbe5f49e724b5f939e89b3128752f9af6555e47
jonlim1994 <jonlim1994@gmail.com>
2018-01-15 17:08:23 +0800

    Added spinner and pcap by device

--- app/src/main/java/com/app/whiff/whiff/help.java
+++ app/src/main/java/com/app/whiff/whiff/help.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.jnetpcap.Pcap;
@@ -23,20 +26,40 @@ public class help extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
-        Button devButton = (Button)findViewById(R.id.getDevButton);
-        devButton.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
+        Spinner devSpinner;
+        ArrayAdapter devList;
+        ArrayList<String> devStringList=new ArrayList<String>();
+        final String[] listDev = new String[3];
+        listDev[0] = "su";
+        listDev[1] = "cd /system/bin";
+        listDev[2] = "tcpdump -D";
 
-                        String[] listDev = new String[3];
-                        listDev[0] = "su";
-                        listDev[1] = "cd /system/bin";
-                        listDev[2] = "tcpdump -D";
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process proc = rt.exec(listDev);
 
-                        TextView mainText = (TextView) findViewById(R.id.textView2);
-                        mainText.setText(callCmd(listDev));
-                    }
-                });
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(proc.getInputStream()));
+
+            String temp;
+            while ((temp = stdInput.readLine()) != null) {
+                int dotPos = temp.indexOf(".");
+                temp = temp.substring(dotPos + 1, temp.length());
+                int spacePos = temp.indexOf(" ");
+                if (spacePos > 0) {
+                    temp = temp.substring(0, spacePos);
+                }
+                devStringList.add(temp);
+            }
+        } catch (IOException e) {
+            Log.i("exception", e.toString());
+        }
+
+        devList = new ArrayAdapter(this, android.R.layout.simple_list_item_1, devStringList);
+        devSpinner = (Spinner)findViewById(R.id.devSpinner);
+        devSpinner.setAdapter(devList);
+        final String selectedDev = devSpinner.getSelectedItem().toString();
+
 
         Button pcapButton = (Button)findViewById(R.id.pcapButton);
         pcapButton.setOnClickListener(
@@ -46,7 +69,7 @@ public class help extends AppCompatActivity {
                         String[] pcap = new String[3];
                         pcap[0] = "su";
                         pcap[1] = "cd /system/bin";
-                        pcap[2] = "tcpdump -s 0 -c 5";
+                        pcap[2] = "tcpdump -i " + selectedDev + " -s 0 -c 5";
 
                         TextView mainText = (TextView) findViewById(R.id.textView2);
                         mainText.setText(callCmd(pcap));
